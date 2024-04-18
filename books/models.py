bb226b0afd0bc6dd62b2472ec4f6fb65e6860a44
Md. Fahim Bhuiyan <67233830+md-fahim-bhuiyan@users.noreply.github.com>
2024-04-18 11:28:36 +0600

    first commit

---
+++ books/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    biography = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.TextField()
+    published_date = models.DateField()
+
+    def __str__(self):
+        return self.title
